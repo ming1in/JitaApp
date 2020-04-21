e6b96a368b152b60b10e676f35c209f718b14fcb
Tom Ripp <unk1222@gmail.com>
2020-04-21 12:24:25 -0400

    Added Firebase info to App

--- Jita/App.js
+++ Jita/App.js
@@ -1,14 +1,32 @@
-/*
-The main app exported to the phone. This should not be changed be changed
-unless we need to edit how the app initialises
+//Firebase was giving me a hard time on my emulator. Looks like andorid didnt
+//like me using fierbase without downloading it for android studio
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+//import firebase from 'react-native-firebase'
+
+/* Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA_aIGfEqpbQwTI6NwboN_NwSYoKgNwfW4",
+  authDomain: "jita-bc882.firebaseapp.com",
+  databaseURL: "https://jita-bc882.firebaseio.com",
+  projectId: "jita-bc882",
+  storageBucket: "jita-bc882.appspot.com",
+  messagingSenderId: "894428466715",
+  appId: "1:894428466715:web:3028cd3f799084a4c032be",
+  measurementId: "G-MS5EB89E5T"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 */
 
-import React, {Component} from 'react';
-import Expo from 'expo';
-import Navigator from './routes/MainRoute'
+import Navigator from './navigation/Navigator'
 
-export default function App(){
+export default function App() {
   return (
-    <Navigator />
-  )
+      <Navigator/>
+  );
 }
+
+const styles = StyleSheet.create({
+});
