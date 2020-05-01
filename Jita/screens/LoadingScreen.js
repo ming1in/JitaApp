a36f61c966a9faaa4d8e1cc4b268ae44a3b0504a
Ali Alobaidi <alinawar2000@gmail.com>
2020-04-30 21:00:07 -0400

    Working state

--- Jita/screens/LoadingScreen.js
+++ Jita/screens/LoadingScreen.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { View, Text, ActivityIndicator, StyleSheet } from 'react-native'
+import firebase from "firebase";
 
 
 export default class Loading extends React.Component {
    componentDidMount() {
       firebase.auth().onAuthStateChanged(user => {
-         this.props.navigation.navigate(user ? 'Main' : 'SignUp')
+         this.props.navigation.navigate(user ? 'Home' : 'Auth')
       })
    }
 
